1a28d72afc14426766ab621618a3474b0a27db35
Duncanian <ianduncan08@gmail.com>
2023-09-14 23:13:04 +0300

    Create landing page (#6)
    Co-authored-by: Ian Abidha <ianabidha@microsoft.com>

--- src/screens/Landing/index.tsx
+++ src/screens/Landing/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { pbClient } from "../../index";
+import companyLogo from "./YaliWall.png";
+import "./style.css";
 
 function initLoginPopup() {
   pbClient.initLoginPopup();
@@ -8,11 +10,16 @@ function initLoginPopup() {
 const Landing = (): JSX.Element => {
   return (
     <div className="landing">
-      Landing Page {pbClient.isLoggedIn ? "Logged In" : "Logged Out"}
-      Landing Page {pbClient.user?.email}
-      <button className="btn btn-primary" onClick={initLoginPopup}>
-        Login
-      </button>
+      <div className="landing-box">
+        Landing Page {pbClient.isLoggedIn ? "Logged In" : "Logged Out"}
+        Landing Page {pbClient.user?.email}
+        <h1 className="landing-header">YALI Connect</h1>
+        {/* <img src="../../icons/Landing/Wall YALI.png" alt="" /> */}
+        <img src={companyLogo} width={"50%"} alt="Works" />
+        <button className="landing-connect-button" onClick={initLoginPopup}>
+          Connect
+        </button>
+      </div>
     </div>
   );
 };
