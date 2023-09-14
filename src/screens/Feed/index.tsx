dee6ec6761cf4d4183234c67fc0c9556f1922ded
Otieno Rowland <rotieno@microsoft.com>
2023-09-15 00:49:19 +0300

    read posts and user profile

--- src/screens/Feed/index.tsx
+++ src/screens/Feed/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Flex, Button } from "@chakra-ui/react";
 import FeedCard from '../../components/FeedCard';
 import CreatePost from '../../components/CreatePost';
@@ -6,6 +6,7 @@ import Leftsidecard from '../../components/Leftsidecard';
 import Layout from '../Layout';
 
 import { pbClient } from "../../index";
+import { Post } from '../../types';
 
 function logout() {
   console.log("logout");
@@ -14,23 +15,46 @@ function logout() {
 }
 
 export const Feed = ({ user }): JSX.Element => {
+  const [isFirstLoad, setIsFirstLoad] = useState(false);
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    if (isFirstLoad) {
+      fetchPosts();
+      //setIsFirstLoad(true);
+    }
+  }, [isFirstLoad, posts]);
+
+  async function fetchPosts() {
+    try {
+      pbClient.postsObservable.subscribe((posts) => {
+        console.log(posts)
+        setPosts(posts);
+      })
+        ;
+    } catch (error) {
+      console.error('Error fetching posts:', error);
+    }
+  }
   return (
     <div>
       <Layout>
         <Flex justifyContent={"space-around"} width={"100%"} pt={"2%"}>
           <Flex width={"80%"}>
             <Box flex={1}>
-              <Leftsidecard />
+              <Leftsidecard user />
             </Box>
             <Box flex={2} px={"2%"}>
               <CreatePost />
-              <FeedCard />
+              {posts.map((post) => (
+                <FeedCard post />
+              ))}
               <Button className="btn btn-primary" onClick={logout}>
                 Logout
               </Button>
             </Box>
             <Box flex={1}>
-              <Leftsidecard />
+              <Leftsidecard user />
             </Box>
           </Flex>
         </Flex>
